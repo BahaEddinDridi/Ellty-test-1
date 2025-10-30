3052d792306cf9696fbba39f1b3d63686b323389
BahaEddinDridi <bahaeddine170@gmail.com>
2025-10-30 13:56:26 +0100

    feat: create Button component with hover/active states

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
 
   return (
     <>
       <h1>Ellty-test</h1>
+      <HomePage />
     </>
   )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../components/Button";
+
+const HomePage: React.FC = () => {
+
+  return (
+    <div className="max-w-[400px] mx-auto mt-[32px]">
+      <div className="mt-[24px]">
+        <Button />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
